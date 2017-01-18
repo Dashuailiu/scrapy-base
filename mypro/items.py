1e3fc8a0a9d06495e36b0def5c3100b035e0404c
dashuai_liu <dashuai.liu@hypers.com>
2017-01-18 14:00:24 +0800

    first commit

---
+++ mypro/items.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MyproItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class DmozItem(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
+
+    def is_null(self):
+        if self['title'] and self['link'] and self['desc']:
+            return False
+        else:
+            return True
+
+
+
---
+++ mypro/spiders/dmoz_spider.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from mypro.items import DmozItem
+
+
+def strip_res(str_list):
+    if str_list:
+        return str_list[0].strip()
+    else:
+        return ""
+
+
+class DomzSpider(scrapy.Spider):
+    name = 'dmoz'
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        'http://www.dmoz.org/Arts/Architecture/Archives/',
+        'http://www.dmoz.org/Arts/Crafts/Paper/Origami/Origamic_Architecture/'
+    ]
+
+    def parse(self, response):
+        filename = response.url.split('/')[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        for sel in response.selector.xpath(
+                "//div[normalize-space(@id)='site-list-content'][normalize-space(@class)='results browse-content']"
+                "/div[normalize-space(@class)='site-item']"
+                "/div[normalize-space(@class)='title-and-desc']"):
+            item = DmozItem()
+            item['title'] = strip_res(sel.xpath("a/div/text()").extract())
+            item['link'] = strip_res(sel.xpath("a/@href").extract())
+            item['desc'] = strip_res(sel.xpath("div[normalize-space(@class)='site-descr']/text()").extract())
+            if not item.is_null():
+                yield item
+
+
